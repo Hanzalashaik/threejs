2b3e02d287ee8a925d02993e56019f6009665fc0
mohammad-oi <mohammad@oneimmersive.us>
2024-10-02 20:30:52 +0530

    materials, geometery, rotation, position

--- my-threejs-app/main.js
+++ my-threejs-app/main.js
@@ -5,25 +5,75 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // Create a scene
 const scene = new THREE.Scene();
 
-// Create a cube geometry and material
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
+//create custom geometery
+// const bufferGeometry = new THREE.BufferGeometry();
 
+// const vertices = new Float32Array([0, 0, 0, 0, 2, 0, 0, 0, 2]);
+// const BufferAttribute = new THREE.BufferAttribute(vertices, 3);
+
+// bufferGeometry.setAttribute("position", BufferAttribute);
+
+// Create a cube geometry and material
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "limeGreen",
+  // wireframe: true,
+  transparent: true,
+  opacity: 0.5,
+});
 // Create a mesh with the geometry and material
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+//Rotation stuff
+// ****
+// cubeMesh.rotation.reorder("YXZ");
+
+// cubeMesh.rotation.x = -THREE.MathUtils.degToRad(45);
+// cubeMesh.rotation.y = -THREE.MathUtils.degToRad(45);
+
+// ***
+
+// Add axes to the scene
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
+
+//grouping the cubes
+// *****/
+// const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// cubeMesh.position.x = -2;
+// cubeMesh3.position.x = 2;
+
+// const group = new THREE.Group();
+// group.add(cubeMesh);
+// group.add(cubeMesh2);
+// group.add(cubeMesh3);
+
+// scene.add(group);
+
+// cubeMesh.position.z = 2;
+// cubeMesh.position.x = -1;
+// ****
 
 // Add the cube to the scene
 scene.add(cubeMesh);
 
+const aspectRatio = window.innerWidth / window.innerHeight;
+
 // Create a camera with a perspective view
-const camera = new THREE.PerspectiveCamera(
-  60,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  30
-);
+const camera = new THREE.PerspectiveCamera(30, aspectRatio, 0.1, 30);
+
+// Create an orthographic camera
+
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   200
+// );
 camera.position.z = 5;
 scene.add(camera);
 
@@ -31,18 +81,43 @@ scene.add(camera);
 const canvas = document.querySelector("canvas.threejs");
 
 // Create a WebGL renderer and link it to the canvas
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
 const controls = new OrbitControls(camera, canvas);
-console.log(controls);
+// console.log(controls);
 // controls.autoRotate = true;
-// controls.enableDamping = true;
+// controls.autoRotateSpeed = 5;
+controls.enableDamping = true;
 
-// Set the renderer size to match the window size
-renderer.setSize(window.innerWidth, window.innerHeight);
+const clock = new THREE.Clock();
+let previousTime = 0;
 
 function renderLoop() {
-  // controls.update();
+  //animations
+
+  // const currentTime = clock.getElapsedTime();
+  // const deltaTime = currentTime - previousTime;
+  // previousTime = currentTime;
+
+  // cubeMesh.rotation.y += THREE.MathUtils.degToRad(45) * deltaTime * 2;
+  // cubeMesh.rotation.x += THREE.MathUtils.degToRad(45) * deltaTime * 2;
+
+  // cubeMesh.position.x = Math.sin(currentTime);
+  // cubeMesh.position.y = Math.sin(currentTime);
+  // cubeMesh.position.z = Math.sin(currentTime);
+
+  // cubeMesh.scale.x = Math.sin(clock.getElapsedTime());
+
+  //resizing
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  // Set the renderer size to match the window size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // manual doing antialiasing
+  // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderLoop);
 }
